e15861e553fa8a20653f8ec09a72a63d25d167c6
Bernhard Herzog <bh@intevation.de>
2001-01-31 16:24:22 +0000

    * keysmenu.h: Remove the prototypes of the functions deleted from 	keysmenu.c

--- src/keysmenu.h
+++ src/keysmenu.h
@@ -1,5 +1,5 @@
 /* keysmenu.h  -  The GNU Privacy Assistant
- *      Copyright (C) 2000 G-N-U GmbH.
+ *      Copyright (C) 2000, 2001 G-N-U GmbH.
  *
  * This file is part of GPA
  *
@@ -24,9 +24,7 @@ extern void keys_unselectKey (GtkWidget * clistKeys, gint row, gint column,
 			      GdkEventButton * event, gpointer param);
 extern GtkWidget *gpa_tableKey_new (GpapaKey * key, GtkWidget * window);
 
-extern void keys_openPublic (void);
 extern void keys_openSecret (void);
-extern void keys_generateKey (void);
 extern void keys_generateRevocation (void);
 extern void keys_import (void);
 extern void keys_importOwnertrust (void);
