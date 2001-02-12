5901336309e4dc1d6bcbc0c9c93c69ab83b0870e
Bernhard Herzog <bh@intevation.de>
2001-02-12 13:54:48 +0000

    * keylist.h (GPAKeyListColumn): 	* keylist.c (column_defs): 	(get_key_type_pixmap_value): New value func and column defintion 	to return a pixmap indicating the type of key: public only or 	secret+public.
    	* keylist.c (ColumnValueFunc):
    	(ColumnDef):
    	(get_name_value):
    	(get_trust_value):
    	(get_ownertrust_value):
    	(get_expirydate_value):
    	(get_identifier_value): Change the prototype of ColumnValueFunc
    	and update all value functions accordingly. Now the value may be a
    	pixmap, but all existing functions only rerturn text as before.
    	The function itself now also indicates whether the text has to be
    	freed.
    	(keylist_row_labels):
    	(keylist_free_row_labels): Removed.
    	(keylist_fill_row): New function to fill a row with the
    	labels/pixmaps for one key

--- src/keylist.c
+++ src/keylist.c
@@ -30,10 +30,15 @@
 
 struct _GPAKeyList;
 
-/* A column definition. A title and a function that returns the value
- * for that column given a public key */
-typedef gchar * (*ColumnValueFunc)(GpapaPublicKey * key,
-				   struct _GPAKeyList * keylist);
+/* A column definition. Consists of a title and a function that returns
+ * the values for that column given a public key */
+
+typedef void (*ColumnValueFunc)(GpapaPublicKey * key,
+				struct _GPAKeyList * keylist,
+				gchar ** label,
+				gboolean * free_label,
+				GdkPixmap ** pixmap,
+				GdkBitmap ** mask);
 
 typedef struct {
   /* column title */
@@ -41,9 +46,6 @@ typedef struct {
 
   /* function to return the value */
   ColumnValueFunc value_func;
-
-  /* if true, the return value has to be freeed. */
-  gboolean free;
 } ColumnDef;
 
 
@@ -73,54 +75,120 @@ typedef struct _GPAKeyList GPAKeyList;
 static void keylist_free (gpointer param);
 static void keylist_fill_list (GPAKeyList * keylist, gboolean keep_selection);
 static void keylist_fill_column_titles (GPAKeyList * keylist);
-static void keylist_row_labels (GPAKeyList * keylist, GpapaPublicKey * key,
-				gchar ** labels);
-static void keylist_free_row_labels (GPAKeyList * keylist, gchar ** labels);
+static void keylist_fill_row (GPAKeyList * keylist, GpapaPublicKey * key,
+			      gint row);
 
 
 /*
  * ColumnDef functions
  */
 
-static gchar *
-get_name_value (GpapaPublicKey * key, GPAKeyList * keylist)
+static void
+get_name_value (GpapaPublicKey * key, GPAKeyList * keylist,
+		gchar ** label, gboolean * free_label, GdkPixmap ** pixmap,
+		GdkBitmap ** mask)
 {
-  return gpapa_key_get_name (GPAPA_KEY (key), gpa_callback, keylist->window);
+  *label = gpapa_key_get_name (GPAPA_KEY (key), gpa_callback, keylist->window);
+  *free_label = FALSE;
+  *pixmap = NULL;
+  *mask = NULL;
 }
 
-static gchar *
-get_trust_value (GpapaPublicKey * key, GPAKeyList * keylist)
+static void
+get_trust_value (GpapaPublicKey * key, GPAKeyList * keylist,
+		 gchar ** label, gboolean * free_label, GdkPixmap ** pixmap,
+		 GdkBitmap ** mask)
 {
   GpapaKeytrust trust;
 
   trust = gpapa_public_key_get_keytrust (key, gpa_callback, keylist->window);
-  return gpa_keytrust_string (trust);
+  *label = gpa_keytrust_string (trust);
+  *free_label = FALSE;
+  *pixmap = NULL;
+  *mask = NULL;
 }
   
-static gchar *
-get_ownertrust_value (GpapaPublicKey * key, GPAKeyList * keylist)
+static void
+get_ownertrust_value (GpapaPublicKey * key, GPAKeyList * keylist,
+		      gchar ** label, gboolean * free_label,
+		      GdkPixmap ** pixmap, GdkBitmap ** mask)
 {
   GpapaOwnertrust trust;
 
   trust = gpapa_public_key_get_ownertrust (key, gpa_callback, keylist->window);
-  return gpa_ownertrust_string (trust);
+  *label = gpa_ownertrust_string (trust);
+  *free_label = FALSE;
+  *pixmap = NULL;
+  *mask = NULL;
 }
 
-static gchar *
-get_expirydate_value (GpapaPublicKey * key, GPAKeyList * keylist)
+static void
+get_expirydate_value (GpapaPublicKey * key, GPAKeyList * keylist,
+		      gchar ** label, gboolean * free_label,
+		      GdkPixmap ** pixmap, GdkBitmap ** mask)
 {
   GDate * date;
 
   date = gpapa_key_get_expiry_date (GPAPA_KEY (key), gpa_callback,
 				    keylist->window);
-  return gpa_expiry_date_string (date);
+  *label = gpa_expiry_date_string (date);
+  *free_label = TRUE;
+  *pixmap = NULL;
+  *mask = NULL;
 }
 
-static gchar *
-get_identifier_value (GpapaPublicKey * key, GPAKeyList * keylist)
+static void
+get_identifier_value (GpapaPublicKey * key, GPAKeyList * keylist,
+		      gchar ** label, gboolean * free_label,
+		      GdkPixmap ** pixmap, GdkBitmap ** mask)
 {
-  return gpapa_key_get_identifier (GPAPA_KEY (key), gpa_callback,
-				   keylist->window);
+  *label = gpapa_key_get_identifier (GPAPA_KEY (key), gpa_callback,
+				     keylist->window);
+  *free_label = FALSE;
+  *pixmap = NULL;
+  *mask = NULL;
+}
+
+static void
+get_key_type_pixmap_value (GpapaPublicKey * key, GPAKeyList * keylist,
+			   gchar ** label, gboolean * free_label,
+			   GdkPixmap ** pixmap, GdkBitmap ** mask)
+{
+  gchar * key_id;
+  GpapaSecretKey * secret_key;
+  static gboolean pixmaps_created = FALSE;
+  static GdkPixmap * secret_pixmap = NULL;
+  static GdkBitmap * secret_mask = NULL;
+  static GdkPixmap * public_pixmap = NULL;
+  static GdkBitmap * public_mask = NULL;
+
+  if (!pixmaps_created)
+    {
+      secret_pixmap = gpa_create_icon_pixmap (keylist->window,
+					      "gpa_yellow_key",
+					      &secret_mask);
+      public_pixmap = gpa_create_icon_pixmap (keylist->window,
+					      "gpa_blue_key",
+					      &public_mask);
+      pixmaps_created = TRUE;
+    }
+
+  key_id = gpapa_key_get_identifier (GPAPA_KEY (key), gpa_callback,
+				     keylist->window);
+  secret_key = gpapa_get_secret_key_by_ID (key_id, gpa_callback,
+					   keylist->window);
+  *label = NULL;
+  *free_label = FALSE;
+  if (secret_key)
+    {
+      *pixmap = secret_pixmap;
+      *mask = secret_mask;
+    }
+  else
+    {
+      *pixmap = public_pixmap;
+      *mask = public_mask;
+    }
 }
 
 /* The order of items in column_defs must match the one in the
@@ -128,11 +196,12 @@ get_identifier_value (GpapaPublicKey * key, GPAKeyList * keylist)
  * into column_defs.
  */
 static ColumnDef column_defs[] = {
-  {N_("User Name"), get_name_value, 0},
-  {N_("Key ID"), get_identifier_value, 0},
-  {N_("Expiry Date"), get_expirydate_value, 1},
-  {N_("Key Trust"), get_trust_value, 0},
-  {N_("Owner Trust"), get_ownertrust_value, 0},
+  {N_("User Name"), get_name_value},
+  {N_("Key ID"), get_identifier_value},
+  {N_("Expiry Date"), get_expirydate_value},
+  {N_("Key Trust"), get_trust_value},
+  {N_("Owner Trust"), get_ownertrust_value},
+  {"", get_key_type_pixmap_value},
 };
 
 
@@ -199,9 +268,9 @@ keylist_fill_list (GPAKeyList * keylist, gboolean keep_selection)
 {
   gint num_keys;
   gint key_index;
-  gint row;
+  gint row, col;
   GpapaPublicKey * key;
-  gchar ** labels = xmalloc (keylist->max_columns * sizeof (gchar*));
+  gchar ** labels;
   gchar * key_id;
   GHashTable * sel_hash = g_hash_table_new (g_str_hash, g_str_equal);
 
@@ -221,6 +290,12 @@ keylist_fill_list (GPAKeyList * keylist, gboolean keep_selection)
 	}
     }
 
+  /* Fill a dummy list of labels. */
+  labels = xmalloc (keylist->max_columns * sizeof (gchar*));
+  for (col = 0; col < keylist->max_columns; col++)
+    labels[col] = "";
+
+  /* Clear the list and fill it again from gpapa's public key list */
   gtk_clist_freeze (GTK_CLIST (keylist->clist));
   gtk_clist_clear (GTK_CLIST (keylist->clist));
   num_keys = gpapa_get_public_key_count(gpa_callback, keylist->window);
@@ -229,13 +304,12 @@ keylist_fill_list (GPAKeyList * keylist, gboolean keep_selection)
       key = gpapa_get_public_key_by_index (key_index, gpa_callback,
 					   keylist->window);
 
-      keylist_row_labels (keylist, key, labels);
       row = gtk_clist_append (GTK_CLIST (keylist->clist), labels);
+      keylist_fill_row (keylist, key, row);
       key_id = gpapa_key_get_identifier (GPAPA_KEY (key), gpa_callback,
 					 keylist->window);
       gtk_clist_set_row_data_full (GTK_CLIST (keylist->clist), row,
 				   xstrdup (key_id), free);
-      keylist_free_row_labels (keylist, labels);
       if (keep_selection && g_hash_table_lookup (sel_hash, key_id))
 	{
 	  /* The key had been selected previously, so select it again */
@@ -252,37 +326,33 @@ keylist_fill_list (GPAKeyList * keylist, gboolean keep_selection)
 } /* keylist_fill_list */
 
 
-/* Fill labels with the labels for the row of the given key. labels must
- * have max_column elements. The elements past keylist->ncolumns are set
- * to empty strings
+/* Fill the clist row row with the labels and/or pixmaps of the given
+ * key.
  */
 static void
-keylist_row_labels (GPAKeyList * keylist, GpapaPublicKey * key,
-		    gchar ** labels)
+keylist_fill_row (GPAKeyList * keylist, GpapaPublicKey * key, gint row)
 {
   gint col;
-
+  gchar * label;
+  gboolean free_label;
+  GdkPixmap * pixmap;
+  GdkBitmap * mask;
+  
   for (col = 0; col < keylist->ncolumns; col++)
     {
-      labels[col] = keylist->column_defs[col]->value_func(key, keylist);
-    }
-  for (col = keylist->ncolumns; col < keylist->max_columns; col++)
-    {
-      labels[col] = "";
-    }
-}
-
-
-/* Free those label strings that were allocated in keylist_row_labels */
-static void
-keylist_free_row_labels (GPAKeyList * keylist, gchar ** labels)
-{
-  gint col;
-
-  for (col = 0; col < keylist->ncolumns; col++)
-    {
-      if (keylist->column_defs[col]->free)
-	free (labels[col]);
+      keylist->column_defs[col]->value_func(key, keylist, &label, &free_label,
+					    &pixmap, &mask);
+      if (pixmap)
+	{
+	  gtk_clist_set_pixmap (GTK_CLIST (keylist->clist), row, col,
+				pixmap, mask);
+	}
+      else if (label)
+	{
+	  gtk_clist_set_text (GTK_CLIST (keylist->clist), row, col, label);
+	}
+      if (free_label)
+	free (label);
     }
 }
 
