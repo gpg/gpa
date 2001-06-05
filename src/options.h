141a3a0e239ae5b0518707208040833bbde64ca2
Werner Koch <wk@gnupg.org>
2001-06-05 12:54:25 +0000

    Add some missing files and one configuration change for W32.  This make up v0.4.2

---
+++ src/options.h
@@ -0,0 +1,35 @@
+/* options.h - global option declarations
+ *	Copyright (C) 2000, 2001 G-N-U GmbH.
+ *
+ * This file is part of GPA
+ *
+ * GPA is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * GPA is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA
+ */
+
+#ifndef OPTIONS_H
+#define OPTIONS_H
+
+typedef struct {
+    int verbose;
+    int quiet;
+    unsigned int debug;
+    char *homedir;
+} GPAOptions;
+
+extern GPAOptions gpa_options;
+
+
+#endif /*OPTIONS_H*/
+
