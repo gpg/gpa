cd9791978b80d708da4ddaff8e8b8fc2b32e4f17
Miguel Coca <mcoca@gnu.org>
2002-09-11 13:21:18 +0000

    * gpgmeedit.c (edit_sign_fnc_action, edit_sign_fnc_transit) (edit_sign_fnc): Use an enum for the state names. Minor fixes.

--- src/gpgmeedit.c
+++ src/gpgmeedit.c
@@ -195,9 +195,23 @@ edit_ownertrust_fnc (void *opaque, GpgmeStatusCode status, const char *args,
 
 /* Sign a key */
 
+
+typedef enum
+{
+  SIGN_START,
+  SIGN_COMMAND,
+  SIGN_UIDS,
+  SIGN_SET_EXPIRE,
+  SIGN_SET_CHECK_LEVEL,
+  SIGN_CONFIRM,
+  SIGN_QUIT,
+  SIGN_SAVE,
+  SIGN_ERROR
+} SignState;
+
 struct sign_parms_s
 {
-  int state;
+  SignState state;
   gchar *check_level;
   gboolean local;
   GpgmeError err;
@@ -209,35 +223,35 @@ edit_sign_fnc_action (struct sign_parms_s *parms, const char **result)
   switch (parms->state)
     {
       /* Start the operation */
-    case 1:
+    case SIGN_COMMAND:
       *result = parms->local ? "lsign" : "sign";
       break;
       /* Sign all user ID's */
-    case 2:
+    case SIGN_UIDS:
       *result = "Y";
       break;
       /* The signature expires at the same time as the key */
-    case 3:
+    case SIGN_SET_EXPIRE:
       *result = "Y";
       break;
       /* Set the check level on the key */
-    case 4:
+    case SIGN_SET_CHECK_LEVEL:
       *result = parms->check_level;
       break;
       /* Confirm the signature */
-    case 5:
+    case SIGN_CONFIRM:
       *result = "Y";
       break;
       /* End the operation */
-    case 6:
+    case SIGN_QUIT:
       *result = "quit";
       break;
       /* Save */
-    case 7:
+    case SIGN_SAVE:
       *result = "Y";
       break;
       /* Special state: an error ocurred. Do nothing until we can quit */
-    case 8:
+    case SIGN_ERROR:
       break;
       /* Can't happen */
     default:
@@ -252,140 +266,135 @@ edit_sign_fnc_transit (struct sign_parms_s *parms, GpgmeStatusCode status,
 {
   switch (parms->state)
     {
-    case 0:
+    case SIGN_START:
       if (status == GPGME_STATUS_GET_LINE &&
           g_str_equal (args, "keyedit.prompt"))
         {
-          parms->state = 1;
-        }
-      else if (status == GPGME_STATUS_KEYEXPIRED)
-        {
-          /* Skip every "signature by an expired key" status line */
-          parms->state = 0;
+          parms->state = SIGN_COMMAND;
         }
       else
         {
-          parms->state = 8;
+          parms->state = SIGN_ERROR;
           parms->err = GPGME_General_Error;
         }
       break;
-    case 1:
+    case SIGN_COMMAND:
       if (status == GPGME_STATUS_GET_BOOL &&
           g_str_equal (args, "keyedit.sign_all.okay"))
         {
-          parms->state = 2;
+          parms->state = SIGN_UIDS;
         }
       else if (status == GPGME_STATUS_GET_LINE &&
                g_str_equal (args, "sign_uid.expire"))
         {
-          parms->state = 3;
+          parms->state = SIGN_SET_EXPIRE;
           break;
         }
       else if (status == GPGME_STATUS_GET_LINE &&
                g_str_equal (args, "sign_uid.class"))
         {
-          parms->state = 4;
+          parms->state = SIGN_SET_CHECK_LEVEL;
         }
       else if (status == GPGME_STATUS_GET_LINE &&
           g_str_equal (args, "keyedit.prompt"))
         {
           /* Failed sign: expired key */
-          parms->state = 8;
+          parms->state = SIGN_ERROR;
           parms->err = GPGME_Invalid_Key;
         }
       else
         {
-          parms->state = 8;
+          parms->state = SIGN_ERROR;
           parms->err = GPGME_General_Error;
         }
       break;
-    case 2:
+    case SIGN_UIDS:
       if (status == GPGME_STATUS_GET_LINE &&
           g_str_equal (args, "sign_uid.expire"))
         {
-          parms->state = 3;
+          parms->state = SIGN_SET_EXPIRE;
           break;
         }
       else if (status == GPGME_STATUS_GET_LINE &&
                g_str_equal (args, "sign_uid.class"))
         {
-          parms->state = 4;
+          parms->state = SIGN_SET_CHECK_LEVEL;
         }
       else if (status == GPGME_STATUS_GET_LINE &&
           g_str_equal (args, "keyedit.prompt"))
         {
           /* Failed sign: expired key */
-          parms->state = 8;
+          parms->state = SIGN_ERROR;
           parms->err = GPGME_Invalid_Key;
         }
       else
         {
-          parms->state = 8;
+          parms->state = SIGN_ERROR;
           parms->err = GPGME_General_Error;
         }
       break;
-    case 3:
+    case SIGN_SET_EXPIRE:
       if (status == GPGME_STATUS_GET_LINE &&
           g_str_equal (args, "sign_uid.class"))
         {
-          parms->state = 4;
+          parms->state = SIGN_SET_CHECK_LEVEL;
         }
       else
         {
-          parms->state = 8;
+          parms->state = SIGN_ERROR;
           parms->err = GPGME_General_Error;
         }
       break;
-    case 4:
+    case SIGN_SET_CHECK_LEVEL:
       if (status == GPGME_STATUS_GET_BOOL &&
           g_str_equal (args, "sign_uid.okay"))
         {
-          parms->state = 5;
+          parms->state = SIGN_CONFIRM;
         }
       else
         {
-          parms->state = 8;
+          parms->state = SIGN_ERROR;
           parms->err = GPGME_General_Error;
         }
       break;
-    case 5:
+    case SIGN_CONFIRM:
       if (status == GPGME_STATUS_GET_LINE &&
           g_str_equal (args, "keyedit.prompt"))
         {
-          parms->state = 6;
+          parms->state = SIGN_QUIT;
         }
       else
         {
-          parms->state = 8;
+          parms->state = SIGN_ERROR;
           parms->err = GPGME_General_Error;
         }
       break;
-    case 6:
+    case SIGN_QUIT:
       if (status == GPGME_STATUS_GET_BOOL &&
           g_str_equal (args, "keyedit.save.okay"))
         {
-          parms->state = 7;
+          parms->state = SIGN_SAVE;
         }
       else
         {
-          parms->state = 8;
+          parms->state = SIGN_ERROR;
           parms->err = GPGME_General_Error;
         }
       break;
-    case 8:
+    case SIGN_ERROR:
       if (status == GPGME_STATUS_GET_LINE &&
           g_str_equal (args, "keyedit.prompt"))
         {
           /* Go to quit operation state */
-          parms->state = 6;
+          parms->state = SIGN_QUIT;
         }
       else
         {
-          parms->state = 8;
+          parms->state = SIGN_ERROR;
         }
       break;
     default:
-      parms->state = 8;
+      parms->state = SIGN_ERROR;
       parms->err = GPGME_General_Error;
     }
 }
@@ -402,7 +411,8 @@ edit_sign_fnc (void *opaque, GpgmeStatusCode status, const char *args,
       status == GPGME_STATUS_NEED_PASSPHRASE ||
       status == GPGME_STATUS_GOOD_PASSPHRASE ||
       status == GPGME_STATUS_USERID_HINT ||
-      status == GPGME_STATUS_SIGEXPIRED)
+      status == GPGME_STATUS_SIGEXPIRED ||
+      status == GPGME_STATUS_KEYEXPIRED)
     {
       return parms->err;
     }
@@ -463,7 +473,7 @@ GpgmeError gpa_gpgme_edit_expiry (GpgmeKey key, GDate *date)
 GpgmeError gpa_gpgme_edit_sign (GpgmeKey key, gchar *private_key_fpr,
                                 gboolean local)
 {
-  struct sign_parms_s parms = {0, "0", local, GPGME_No_Error};
+  struct sign_parms_s parms = {SIGN_START, "0", local, GPGME_No_Error};
   GpgmeKey secret_key = gpa_keytable_secret_lookup (keytable, private_key_fpr);
   GpgmeError err = GPGME_No_Error;
   GpgmeData out;
