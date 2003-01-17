dfb301717167b2b12f4ddcfd50235f248c13e420
Miguel Coca <mcoca@gnu.org>
2003-01-17 00:38:57 +0000

    src/ 2003-01-17  Miguel Coca  <mcoca@gnu.org>
    	* keygendlg.c (gpa_key_gen_run_dialog): More UI Fixes.
    	(response_cb): Add check for empty or easy passphrase.

--- src/keygendlg.c
+++ src/keygendlg.c
@@ -24,6 +24,7 @@
 #include "gpawidgets.h"
 #include "gtktools.h"
 #include "keygendlg.h"
+#include "qdchkpwd.h"
 
 #define	XSTRDUP_OR_NULL(s)	((s != NULL) ? g_strdup(s): NULL)
 
@@ -43,16 +44,46 @@ response_cb(GtkDialog *dlg, gint response, gpointer param)
 {
   GPAKeyGenDialog * dialog = param;
   gchar * expiry_error;
+  const gchar *passwd = gtk_entry_get_text (GTK_ENTRY (dialog->entryPasswd));
+  const gchar *repeat = gtk_entry_get_text (GTK_ENTRY (dialog->entryRepeat));
 
   if (response == GTK_RESPONSE_OK)
     {
-      if (strcmp (gtk_entry_get_text (GTK_ENTRY (dialog->entryPasswd)),
-                  gtk_entry_get_text (GTK_ENTRY (dialog->entryRepeat))) != 0)
+      if (!g_str_equal (passwd, repeat))
         {
-          gpa_window_error (_("In \"Passphrase\" and \"Repeat passphrase\"\n"
+          gpa_window_error (_("In \"Passphrase\" and \"Repeat passphrase\",\n"
                               "you must enter the same passphrase."),
                             dialog->window);
           g_signal_stop_emission_by_name (dlg, "response");
+        }
+      else if (strlen (passwd) == 0)
+        {
+          gpa_window_error (_("You did not enter a passphrase.\n"
+                              "It is needed to protect your private key."),
+                            dialog->window);
+          g_signal_stop_emission_by_name (dlg, "response");
+        }
+      else if (strlen (passwd) < 10 || qdchkpwd (passwd) < 0.6)
+        {
+          GtkWidget *msgbox;
+          
+          msgbox = gtk_message_dialog_new (GTK_WINDOW (dialog->window),
+                                           GTK_DIALOG_MODAL,
+                                           GTK_MESSAGE_WARNING,
+                                           GTK_BUTTONS_NONE,
+                                           _("Warning: You have entered a "
+                                             "passphrase\n"
+                                             "that is obviously not secure.\n\n"
+                                             "Please enter a new passphrase."));
+          gtk_dialog_add_buttons (GTK_DIALOG (msgbox), 
+                                  _("_Enter new passphrase"), GTK_RESPONSE_CANCEL,
+                                  _("Take this one _anyway"), GTK_RESPONSE_OK,
+                                  NULL);
+          if (gtk_dialog_run (GTK_DIALOG (msgbox)) == GTK_RESPONSE_CANCEL)
+            {
+              g_signal_stop_emission_by_name (dlg, "response");
+            }
+          gtk_widget_destroy (msgbox);
         }
       else if ((expiry_error = gpa_expiry_frame_validate (dialog->frameExpire)))
         {
@@ -75,13 +106,12 @@ gpa_key_gen_run_dialog (GtkWidget * parent)
   GtkAccelGroup *accelGroup;
   GtkWidget *windowGenerate;
   GtkWidget *vboxGenerate;
-  GtkWidget *tableTop;
+  GtkWidget *table;
   GtkWidget *labelAlgorithm;
   GtkWidget *comboAlgorithm;
   GtkWidget *labelKeysize;
   GtkWidget *comboKeysize;
   GtkWidget *frameExpire;
-  GtkWidget *tableMisc;
   GtkWidget *labelUserID;
   GtkWidget *entryUserID;
   GtkWidget *labelEmail;
@@ -92,7 +122,6 @@ gpa_key_gen_run_dialog (GtkWidget * parent)
   GtkWidget *entryPasswd;
   GtkWidget *labelRepeat;
   GtkWidget *entryRepeat;
-  GtkWidget *vboxMisc;
 
   GPAKeyGenDialog dialog;
   GPAKeyGenParameters * params = NULL;
@@ -126,13 +155,13 @@ gpa_key_gen_run_dialog (GtkWidget * parent)
   gtk_container_add (GTK_CONTAINER (windowGenerate), vboxGenerate);
   gtk_container_set_border_width (GTK_CONTAINER (vboxGenerate), 5);
 
-  tableTop = gtk_table_new (2, 2, FALSE);
-  gtk_container_set_border_width (GTK_CONTAINER (tableTop), 5);
-  gtk_box_pack_start (GTK_BOX (vboxGenerate), tableTop, FALSE, FALSE, 0);
+  table = gtk_table_new (7, 2, FALSE);
+  gtk_container_set_border_width (GTK_CONTAINER (table), 5);
+  gtk_box_pack_start (GTK_BOX (vboxGenerate), table, FALSE, FALSE, 0);
 
   labelAlgorithm = gtk_label_new ("");
   gtk_misc_set_alignment (GTK_MISC (labelAlgorithm), 1.0, 0.5);
-  gtk_table_attach (GTK_TABLE (tableTop), labelAlgorithm, 0, 1, 0, 1,
+  gtk_table_attach (GTK_TABLE (table), labelAlgorithm, 0, 1, 0, 1,
 		    GTK_FILL, GTK_SHRINK, 0, 0);
   comboAlgorithm = gtk_combo_new ();
   gtk_editable_set_editable (GTK_EDITABLE (GTK_COMBO (comboAlgorithm)->entry),
@@ -145,12 +174,12 @@ gpa_key_gen_run_dialog (GtkWidget * parent)
   gpa_connect_by_accelerator (GTK_LABEL (labelAlgorithm),
 			      GTK_COMBO (comboAlgorithm)->entry, accelGroup,
 			      _("_Algorithm: "));
-  gtk_table_attach (GTK_TABLE (tableTop), comboAlgorithm, 1, 2, 0, 1,
+  gtk_table_attach (GTK_TABLE (table), comboAlgorithm, 1, 2, 0, 1,
 		    GTK_FILL, GTK_SHRINK, 0, 0);
 
   labelKeysize = gtk_label_new ("");
   gtk_misc_set_alignment (GTK_MISC (labelKeysize), 1.0, 0.5);
-  gtk_table_attach (GTK_TABLE (tableTop), labelKeysize, 0, 1, 1, 2,
+  gtk_table_attach (GTK_TABLE (table), labelKeysize, 0, 1, 1, 2,
 		    GTK_FILL, GTK_SHRINK, 0, 0);
   comboKeysize = gtk_combo_new ();
   gtk_combo_set_value_in_list (GTK_COMBO (comboKeysize), FALSE, FALSE);
@@ -162,78 +191,70 @@ gpa_key_gen_run_dialog (GtkWidget * parent)
   contentsKeysize = g_list_append (contentsKeysize, _("2048"));
   gtk_combo_set_popdown_strings (GTK_COMBO (comboKeysize), contentsKeysize);
   gtk_entry_set_text (GTK_ENTRY (GTK_COMBO (comboKeysize)->entry), "1024");
-  gtk_table_attach (GTK_TABLE (tableTop), comboKeysize, 1, 2, 1, 2,
+  gtk_table_attach (GTK_TABLE (table), comboKeysize, 1, 2, 1, 2,
 		    GTK_FILL, GTK_SHRINK, 0, 0);
   /* FIXME: In the above we should either translate all key sizes
    * (including the one in the gtk_entry_set_text call) or generate all
    * strings via sprintf or a similar locale aware function */
 
-  frameExpire = gpa_expiry_frame_new (accelGroup, NULL);
-  dialog.frameExpire = frameExpire;
-  gtk_container_set_border_width (GTK_CONTAINER (frameExpire), 5);
-  gtk_box_pack_start (GTK_BOX (vboxGenerate), frameExpire, FALSE, FALSE, 0);
-
-  tableMisc = gtk_table_new (5, 2, FALSE);
-  gtk_box_pack_start (GTK_BOX (vboxGenerate), tableMisc, TRUE, FALSE, 0);
-  gtk_container_set_border_width (GTK_CONTAINER (tableMisc), 5);
-
   labelUserID = gtk_label_new ("");
   gtk_misc_set_alignment (GTK_MISC (labelUserID), 1.0, 0.5);
-  gtk_table_attach (GTK_TABLE (tableMisc), labelUserID, 0, 1, 0, 1,
+  gtk_table_attach (GTK_TABLE (table), labelUserID, 0, 1, 2, 3,
 		    GTK_FILL, GTK_SHRINK, 0, 0);
   entryUserID = gtk_entry_new ();
   gpa_connect_by_accelerator (GTK_LABEL (labelUserID), entryUserID,
 			      accelGroup, _("_User ID: "));
-  gtk_table_attach (GTK_TABLE (tableMisc), entryUserID, 1, 2, 0, 1, GTK_FILL,
+  gtk_table_attach (GTK_TABLE (table), entryUserID, 1, 2, 2, 3, GTK_FILL,
 		    GTK_SHRINK, 0, 0);
 
   labelEmail = gtk_label_new ("");
   gtk_misc_set_alignment (GTK_MISC (labelEmail), 1.0, 0.5);
-  gtk_table_attach (GTK_TABLE (tableMisc), labelEmail, 0, 1, 1, 2,
+  gtk_table_attach (GTK_TABLE (table), labelEmail, 0, 1, 3, 4,
 		    GTK_FILL, GTK_SHRINK, 0, 0);
   entryEmail = gtk_entry_new ();
   gpa_connect_by_accelerator (GTK_LABEL (labelEmail), entryEmail, accelGroup,
 			      _("_Email: "));
-  gtk_table_attach (GTK_TABLE (tableMisc), entryEmail, 1, 2, 1, 2, GTK_FILL,
+  gtk_table_attach (GTK_TABLE (table), entryEmail, 1, 2, 3, 4, GTK_FILL,
 		    GTK_SHRINK, 0, 0);
 
   labelComment = gtk_label_new ("");
   gtk_misc_set_alignment (GTK_MISC (labelComment), 1.0, 0.5);
-  gtk_table_attach (GTK_TABLE (tableMisc), labelComment, 0, 1, 2, 3,
+  gtk_table_attach (GTK_TABLE (table), labelComment, 0, 1, 4, 5,
 		    GTK_FILL, GTK_SHRINK, 0, 0);
   entryComment = gtk_entry_new ();
   gpa_connect_by_accelerator (GTK_LABEL (labelComment), entryComment,
 			      accelGroup, _("_Comment: "));
-  gtk_table_attach (GTK_TABLE (tableMisc), entryComment, 1, 2, 2, 3, GTK_FILL,
+  gtk_table_attach (GTK_TABLE (table), entryComment, 1, 2, 4, 5, GTK_FILL,
 		    GTK_SHRINK, 0, 0);
 
   labelPasswd = gtk_label_new ("");
   gtk_misc_set_alignment (GTK_MISC (labelPasswd), 1.0, 0.5);
-  gtk_table_attach (GTK_TABLE (tableMisc), labelPasswd, 0, 1, 3, 4,
+  gtk_table_attach (GTK_TABLE (table), labelPasswd, 0, 1, 5, 6,
 		    GTK_FILL, GTK_SHRINK, 0, 0);
   entryPasswd = gtk_entry_new ();
   dialog.entryPasswd = entryPasswd;
   gtk_entry_set_visibility (GTK_ENTRY (entryPasswd), FALSE);
   gpa_connect_by_accelerator (GTK_LABEL (labelPasswd), entryPasswd,
 			      accelGroup, _("_Passphrase: "));
-  gtk_table_attach (GTK_TABLE (tableMisc), entryPasswd, 1, 2, 3, 4, GTK_FILL,
+  gtk_table_attach (GTK_TABLE (table), entryPasswd, 1, 2, 5, 6, GTK_FILL,
 		    GTK_SHRINK, 0, 0);
 
   labelRepeat = gtk_label_new ("");
   gtk_misc_set_alignment (GTK_MISC (labelRepeat), 1.0, 0.5);
-  gtk_table_attach (GTK_TABLE (tableMisc), labelRepeat, 0, 1, 4, 5,
+  gtk_table_attach (GTK_TABLE (table), labelRepeat, 0, 1, 6, 7,
 		    GTK_FILL, GTK_SHRINK, 0, 0);
   entryRepeat = gtk_entry_new ();
   dialog.entryRepeat = entryRepeat;
   gtk_entry_set_visibility (GTK_ENTRY (entryRepeat), FALSE);
   gpa_connect_by_accelerator (GTK_LABEL (labelRepeat), entryRepeat,
 			      accelGroup, _("_Repeat passphrase: "));
-  gtk_table_attach (GTK_TABLE (tableMisc), entryRepeat, 1, 2, 4, 5,
+  gtk_table_attach (GTK_TABLE (table), entryRepeat, 1, 2, 6, 7,
 		    GTK_FILL, GTK_SHRINK, 0, 0);
 
-  vboxMisc = gtk_vbox_new (FALSE, 0);
-  gtk_box_pack_start (GTK_BOX (vboxGenerate), vboxMisc, FALSE, FALSE, 0);
-  gtk_container_set_border_width (GTK_CONTAINER (vboxMisc), 5);
+  frameExpire = gpa_expiry_frame_new (accelGroup, NULL);
+  dialog.frameExpire = frameExpire;
+  gtk_container_set_border_width (GTK_CONTAINER (frameExpire), 5);
+  gtk_box_pack_start (GTK_BOX (vboxGenerate), frameExpire, FALSE, FALSE, 0);
 
   gtk_widget_show_all (windowGenerate);
   if (gtk_dialog_run (GTK_DIALOG (windowGenerate)) == GTK_RESPONSE_OK)
