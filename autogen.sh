60585dad48784313984c33aadbc4b74ff68af068
Werner Koch <wk@gnupg.org>
2012-11-16 13:52:04 +0100

    Fix non-portable use of chmod in autogen.sh.
    * autogen.sh: Remove option -c from chmod.

--- autogen.sh
+++ autogen.sh
@@ -214,7 +214,7 @@ if [ -d .git ]; then
     and .git/hooks/pre-commit.sample out of the way.
 EOF
       cp -av .git/hooks/pre-commit.sample .git/hooks/pre-commit
-      chmod -c +x  .git/hooks/pre-commit
+      chmod +x  .git/hooks/pre-commit
   fi
   tmp=$(git config --get filter.cleanpo.clean)
   if [ "$tmp" != "awk '/^\"POT-Creation-Date:/&&!s{s=1;next};!/^#: /{print}'" ]
@@ -228,7 +228,7 @@ EOF
 *** Activating commit log message check hook. ***
 EOF
       cp -av build-aux/git-hooks/commit-msg .git/hooks/commit-msg
-      chmod -c +x  .git/hooks/commit-msg
+      chmod +x  .git/hooks/commit-msg
   fi
 fi
 
