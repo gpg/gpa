7a5b070eb786fb09efe2f8c57784882219ade2d9
Werner Koch <wk@gnupg.org>
2013-07-10 12:08:28 +0200

    Change the license of some files to LGPLv2.1.
    * src/filetype.c: Change license notice
    * src/filetype.h: Ditto.
    * src/parsetlv.c: Ditto.
    * src/parsetlv.h: Ditto.
    --
    
    Those files are useful for other projects and thus we (g10 Code) allow
    its use under the less restrictive LGPLv2.1.
    
    NB: If you think that other parts of the code are also useful for
    projects with different licenses requirements, please write to me.  If
    g10 Code is the sole copyright holder, we may be able to change the
    license for that code as well.

--- src/parsetlv.c
+++ src/parsetlv.c
@@ -1,19 +1,17 @@
 /* parsetlv.c -  ASN.1 TLV functions
  * Copyright (C) 2005, 2007, 2008, 2012 g10 Code GmbH
  *
- * This file is part of GPA
+ * This file is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
  *
- * GPA is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 3 of the License, or
- * (at your option) any later version.
+ * This file is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
  *
- * GPA is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
- * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
- * License for more details.
- *
- * You should have received a copy of the GNU General Public License
+ * You should have received a copy of the GNU Lesser General Public License
  * along with this program; if not, see <http://www.gnu.org/licenses/>.
  */
 
