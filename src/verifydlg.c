ed22ed1f369e06f2d8832ac26ceac035eb482b75
Andreas Rönnquist <gusnan@librem.one>
2022-10-28 21:38:48 +0200

    Fix incompatible pointer type warning

--- src/verifydlg.c
+++ src/verifydlg.c
@@ -120,7 +120,7 @@ gpa_file_verify_dialog_constructor (GType type,
   dialog->notebook = gtk_notebook_new ();
 
   //vboxEncrypt = gtk_dialog_get_content_area(dialog);
-  GtkWidget *box = gtk_dialog_get_content_area(dialog);
+  GtkWidget *box = gtk_dialog_get_content_area (GTK_DIALOG (dialog));
 
   gtk_box_pack_start (GTK_BOX (box),
 			       dialog->notebook, TRUE, TRUE, 0);
