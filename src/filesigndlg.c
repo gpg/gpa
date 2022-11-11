3f87efc905c0944ddb84c372623a8adae152aaca
Andreas Rönnquist <gusnan@librem.one>
2022-11-11 19:13:15 +0100

    Remove deprecated gtk_dialog_set_alternative_button_order

--- src/filesigndlg.c
+++ src/filesigndlg.c
@@ -164,10 +164,6 @@ gpa_file_sign_dialog_constructor (GType type,
 			  GTK_STOCK_CANCEL, GTK_RESPONSE_CANCEL,
 			  GTK_STOCK_OK, GTK_RESPONSE_OK,
                           NULL);
-  gtk_dialog_set_alternative_button_order (GTK_DIALOG (dialog),
-                                           GTK_RESPONSE_OK,
-                                           GTK_RESPONSE_CANCEL,
-                                           -1);
   gpa_window_set_title (GTK_WINDOW (dialog), _("Sign documents"));
   gtk_dialog_set_default_response (GTK_DIALOG (dialog), GTK_RESPONSE_OK);
   gtk_container_set_border_width (GTK_CONTAINER (dialog), 5);
