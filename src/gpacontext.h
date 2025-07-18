b6ba8bcc6db7765667cd6c49b7edc9a2073bc74f
Werner Koch <wk@gnupg.org>
2025-07-18 09:45:23 +0200

    Remove trust_item stuff to make it build with gpgme 2.x
    * src/gpacontext.h (struct _GpaContextClass): Remove next_trust_item.
    * src/gpacontext.c (NEXT_TRUST_ITEM): Remove
    (gpa_context_class_init): Remove assignment.
    (gpa_context_event_cb): Remove handler.
    (gpa_context_next_trust_item): Remove.
    --
    
    Note that the trust item feature worked only for a very short period
    in 2003.

--- src/gpacontext.h
+++ src/gpacontext.h
@@ -63,7 +63,6 @@ struct _GpaContextClass {
   void (*start) (GpaContext *context);
   void (*done) (GpaContext *context, gpg_error_t err);
   void (*next_key) (GpaContext *context, gpgme_key_t key);
-  void (*next_trust_item) (GpaContext *context, gpgme_trust_item_t item);
   void (*progress) (GpaContext *context, int current, int total);
 };
 
