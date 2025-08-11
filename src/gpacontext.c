gpg/gpa

commit 95e07080a2a08196cafb05b69345ea1d629424b1

gtk_mashal_* shouldn't be used

src/gpacontext.c
@@ -158,9 +158,9 @@ gpa_context_class_init (GpaContextClass *klass)
                         G_SIGNAL_RUN_FIRST,
                         G_STRUCT_OFFSET (GpaContextClass, progress),
                         NULL, NULL,
-			gtk_marshal_VOID__INT_INT,
+                        g_cclosure_marshal_VOID__INT,
                         G_TYPE_NONE, 2,
-			G_TYPE_INT, G_TYPE_INT);
+			G_TYPE_INT);
 }
 
 static void

src/gpakeyexpireop.c
@@ -114,7 +114,7 @@ gpa_key_expire_operation_class_init (GpaKeyExpireOperationClass *klass)
 		  G_SIGNAL_RUN_FIRST,
 		  G_STRUCT_OFFSET (GpaKeyExpireOperationClass, new_expiration),
 		  NULL, NULL,
-		  gtk_marshal_VOID__POINTER_POINTER,
+		  g_cclosure_marshal_VOID__POINTER,
 		  G_TYPE_NONE, 2,
 		  G_TYPE_POINTER,
 		  G_TYPE_POINTER);

commit 45fc64a4ac9f73457bf610ce76cca087c0d0d09a

Fix incorrect callback arguments
* src/gpa-marshal.list: Add two entries.
* src/gpacontext.c (gpa_context_class_init): Fix "progress" signal.
* src/gpakeyexpireop.c (gpa_key_expire_operation_class_init):
Use gpa_marshal_VOID__POINTER_POINTER.

--

Some users reported a crash downstream in Gentoo with
g_type_check_is_value_type getting a garbage type. This turns out to
be because 95e07080a2a08196cafb05b69345ea1d629424b1 replaced the
types (and argument counts) incorrectly.

Fix that by adding to gpa_marshal.list to create custom marshal types
and use those instead, and fix the argument count.

Bug: https://bugs.gentoo.org/957196
Fixes: 95e07080a2a08196cafb05b69345ea1d629424b1
Signed-off-by: Sam James <sam@gentoo.org>

ChangeLog entries added by wk.

src/gpa-marshal.list
@@ -1,1 +1,3 @@
 INT:STRING,STRING
+VOID:INT,INT
+VOID:POINTER,POINTER

src/gpacontext.c
@@ -25,6 +25,7 @@
 #include "gpa.h"
 #include "gpgmetools.h"
 #include "gpacontext.h"
+#include "gpa-marshal.h"
 
 /* GObject type functions */
 
@@ -145,9 +146,9 @@ gpa_context_class_init (GpaContextClass *klass)
                         G_SIGNAL_RUN_FIRST,
                         G_STRUCT_OFFSET (GpaContextClass, progress),
                         NULL, NULL,
-                        g_cclosure_marshal_VOID__INT,
+                        gpa_marshal_VOID__INT_INT,
                         G_TYPE_NONE, 2,
-			G_TYPE_INT);
+			G_TYPE_INT, G_TYPE_INT);
 }
 
 static void

src/gpakeyexpireop.c
@@ -31,6 +31,7 @@
 #endif
 
 #include "gpa.h"
+#include "gpa-marshal.h"
 #include "gpakeyexpireop.h"
 #include "expirydlg.h"
 #include "gpgmeedit.h"
@@ -114,7 +115,7 @@ gpa_key_expire_operation_class_init (GpaKeyExpireOperationClass *klass)
 		  G_SIGNAL_RUN_FIRST,
 		  G_STRUCT_OFFSET (GpaKeyExpireOperationClass, new_expiration),
 		  NULL, NULL,
-		  g_cclosure_marshal_VOID__POINTER,
+		  gpa_marshal_VOID__POINTER_POINTER,
 		  G_TYPE_NONE, 2,
 		  G_TYPE_POINTER,
 		  G_TYPE_POINTER);